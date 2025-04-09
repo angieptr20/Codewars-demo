f0a9c2dced41962795cd421e97a808932e427369
Angie Mejia Holguin <angieptr20@gmail.com>
2025-04-08 23:16:50 -0400

    Some more coding challenges

--- removeExclamationMarks.js
+++ removeExclamationMarks.js
@@ -8,3 +8,20 @@ function removeExclamationMarks(s){
 console.log(removeExclamationMarks("Hello! World!!"));  // "Hello World"
 console.log(removeExclamationMarks("Wow!!! This is great!!!"));  // "Wow This is great"
 console.log(removeExclamationMarks("No exclamation mark here"));  // "No exclamation mark here"
+
+//OR
+
+function removeExclamationMarks(s) {
+    let result = '';
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] !== '!') {
+            result += s[i];
+        }
+    }
+    return result;
+}
+
+// Example usage:
+console.log(removeExclamationMarks("Hello! World!!"));  // "Hello World"
+console.log(removeExclamationMarks("Wow!!! This is great!!!"));  // "Wow This is great"
+console.log(removeExclamationMarks("No exclamation mark here"));  // "No exclamation mark here"
---
+++ reverseAString.js
@@ -0,0 +1,11 @@
+//Complete the solution so that it reverses the string passed into it.
+
+function solution(str){
+ 
+    //let word = str.split('').reverse().join('')
+  
+   return str.split('').reverse().join('');
+  }
+  
+  console.log(solution('hey')); // "yeh"
+
